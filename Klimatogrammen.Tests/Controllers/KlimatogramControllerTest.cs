75431002d00a085e501ee785dc024999405eb3d6
Pieter-Jan Vandenberghe <pieterjan.vandenberghe.t6365@student.hogent.be>
2015-02-18 20:25:54 +0100

    #17 Testen controller bestanden fixen

---
+++ Klimatogrammen.Tests/Controllers/KlimatogramControllerTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Klimatogrammen.Infrastructure;
+using System.Web.Mvc;
+using Klimatogrammen.Controllers;
+using Klimatogrammen.ViewModels;
+using System.Collections.Generic;
+using Klimatogrammen.Models.Domein;
+
+namespace Klimatogrammen.Tests.Controllers
+{
+    [TestClass]
+    public class KlimatogramControllerTest
+    {
+        private KlimatogramController klimatogramController;
+        private ISessionRepository _sessionRepository;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _sessionRepository = new SessionRepositoryMock();
+            klimatogramController = new KlimatogramController(_sessionRepository);
+        }
+
+        [TestMethod]
+        public void IndexIsNietNull()
+        {
+            ViewResult result = klimatogramController.Index() as ViewResult;
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void IndexHttpPostRedirectNaarLanden()
+        {
+
+        }
+
+        [TestMethod]
+        public void ContinentZitInSessieNaCorrectePost()
+        {
+            
+        }
+
+        [TestMethod]
+        public void IndexHttpPostRedirectNaarLocaties()
+        {
+            
+        }
+
+        [TestMethod]
+        public void LandZitInSessieNaCorrectePost()
+        {
+            
+        }
+
+        [TestMethod]
+        public void IndexHttpPostRedirectKlimatogram()
+        {
+            
+        }
+
+        [TestMethod]
+        public void KlimatogramZitInSessieNaCorrectePost()
+        {
+            
+        }
+
+    }
+}
