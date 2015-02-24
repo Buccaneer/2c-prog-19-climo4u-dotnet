46dc74fafe73aa9d2e6cc780b6086a43ae7b082b
Pieter-Jan Vandenberghe <pieterjan.vandenberghe.t6365@student.hogent.be>
2015-02-24 20:50:34 +0100

    #11 ViewModel implementeren

---
+++ Klimatogrammen/ViewModels/VragenIndexViewModel.cs
@@ -0,0 +1,50 @@
+﻿using Klimatogrammen.Models.Domein;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Klimatogrammen.ViewModels
+{
+    public class VragenIndexViewModel
+    {
+        public ICollection<VraagViewModel> VraagViewModels { get; set; }
+
+        public VragenIndexViewModel(VraagRepository vRep)
+        {
+            VraagViewModels = new List<VraagViewModel>();
+            foreach(Vraag v in vRep.Vragen)
+            {
+                VraagViewModels.Add(new VraagViewModel(v.GeefVraagTekst(), v.GeefMogelijkeAntwoorden()));
+            }
+        }
+
+    }
+
+    public class VraagViewModel
+    {
+        public string VraagTekst { get; set; }
+        public SelectList Antwoorden { get; set; }
+
+        [Required(ErrorMessage = "Er moet een antwoord geselecteerd worden.")]
+        public string Antwoord { get; set; }
+
+        public bool Resultaat { get; set; }
+
+        public string ValidatieTekst { get; set; }
+
+        public VraagViewModel(string vraag, ICollection<string> antwoorden)
+        {
+            VraagTekst = vraag;
+            Antwoorden = new SelectList(antwoorden, "Antwoord", "Antwoord");
+        }
+
+        public VraagViewModel()
+        {
+
+        }
+
+    }
+}
