580819b26a138e60920924a8611fc26bbae8c8f2
Jasper De Vrient <jasperdevrient@hotmail.com>
2015-03-07 14:40:42 +0100

    #1: hallo

---
+++ Klimatogrammen/Models/Domein/ResultaatBlad.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Klimatogrammen.Models.Domein {
+    /// <summary>
+    /// Stelt een resultaat van een determinatie voor. Heeft een Vegetatietype en een klimaattype.
+    /// </summary>
+    public class ResultaatBlad : DeterminatieKnoop {
+
+        public string VegetatieType { get; private set; }
+
+        public string KlimaatType { get; private set; }
+
+        public override DeterminatieKnoop Determineer(Klimatogram klimatogram) {
+            return this;
+        }
+
+        public ResultaatBlad() { }
+
+        public ResultaatBlad(string vegType, string klimType) {
+            VegetatieType = vegType;
+            KlimaatType = klimType;
+        }
+
+       
+    }
+}
