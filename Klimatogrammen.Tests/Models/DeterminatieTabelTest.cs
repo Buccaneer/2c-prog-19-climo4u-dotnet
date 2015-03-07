a9a0aec17f517556ceba86318d92ea6fe1a48713
Annemie De Groote <miebeertje@hotmail.com>
2015-03-07 22:26:19 +0100

    #3 Testen domein

--- Klimatogrammen.Tests/Models/DeterminatieTabelTest.cs
+++ Klimatogrammen.Tests/Models/DeterminatieTabelTest.cs
@@ -29,39 +29,48 @@ namespace Klimatogrammen.Tests.Models
         }
 
         [TestMethod]
-        public void DeterminatieGebruikerIsFoutief()
+        public void DeterminatieGeeftAlsResultaatJuist()
         {
-            _determinatieTabel.Determineer(_klimatogramMock.Object);
-            Resultaat verwacht = Resultaat.Fout;
-            while (!_determinatieTabel.HeeftLeerlingEindeBereikt())
-                _determinatieTabel.NeemNeeKnoop();
-
-            Assert.AreEqual(verwacht, _determinatieTabel.ValideerGebruiker());
+            ResultaatBlad result = _determinatieTabel.Determineer(_klimatogramMock.Object);
+            Resultaat resultaat = _determinatieTabel.ValideerGebruikerResultaat(result);
+            Assert.AreEqual(resultaat, Resultaat.Juist);
         }
 
-        [TestMethod]
-        public void DeterminatieGebruikerIsJuist()
-        {
-            _determinatieTabel.Determineer(_klimatogramMock.Object);
-            Resultaat verwacht = Resultaat.Juist;
-            _determinatieTabel.NeemNeeKnoop();
-            _determinatieTabel.NeemNeeKnoop();
-            _determinatieTabel.NeemJaKnoop();
-            _determinatieTabel.NeemNeeKnoop();
 
-            Assert.AreEqual(verwacht, _determinatieTabel.ValideerGebruiker());
-        }
+        //[TestMethod]
+        //public void DeterminatieGebruikerIsFoutief()
+        //{
+        //    _determinatieTabel.Determineer(_klimatogramMock.Object);
+        //    Resultaat verwacht = Resultaat.Fout;
+        //    while (!_determinatieTabel.HeeftLeerlingEindeBereikt())
+        //        _determinatieTabel.NeemNeeKnoop();
 
-        [TestMethod]
-        public void FoutieveDeterminatieGebruikerWistZijnPad()
-        {
-            _determinatieTabel.Determineer(_klimatogramMock.Object);
-            var huidige = _determinatieTabel.GeefDeterminatieGebruiker();
-            _determinatieTabel.NeemJaKnoop();
-            _determinatieTabel.ValideerGebruiker();
-            var nu = _determinatieTabel.GeefDeterminatieGebruiker();
+        //    Assert.AreEqual(verwacht, _determinatieTabel.ValideerGebruiker());
+        //}
 
-            Assert.AreEqual(huidige,nu);
-        }
+        //[TestMethod]
+        //public void DeterminatieGebruikerIsJuist()
+        //{
+        //    _determinatieTabel.Determineer(_klimatogramMock.Object);
+        //    Resultaat verwacht = Resultaat.Juist;
+        //    _determinatieTabel.NeemNeeKnoop();
+        //    _determinatieTabel.NeemNeeKnoop();
+        //    _determinatieTabel.NeemJaKnoop();
+        //    _determinatieTabel.NeemNeeKnoop();
+
+        //    Assert.AreEqual(verwacht, _determinatieTabel.ValideerGebruiker());
+        //}
+
+        //[TestMethod]
+        //public void FoutieveDeterminatieGebruikerWistZijnPad()
+        //{
+        //    _determinatieTabel.Determineer(_klimatogramMock.Object);
+        //    var huidige = _determinatieTabel.GeefDeterminatieGebruiker();
+        //    _determinatieTabel.NeemJaKnoop();
+        //    _determinatieTabel.ValideerGebruiker();
+        //    var nu = _determinatieTabel.GeefDeterminatieGebruiker();
+
+        //    Assert.AreEqual(huidige,nu);
+        //}
     }
 }
---
+++ Klimatogrammen.Tests/Models/VergelijkingTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Klimatogrammen.Models.Domein;
+using Klimatogrammen.Tests.Mock;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Klimatogrammen.Tests.Models
+{
+    [TestClass]
+    public class VergelijkingTest
+    {
+        private Mock<Klimatogram> _klimatogramMock;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _klimatogramMock = new KlimatogramMockFactory().MaakKlimatogramMock();
+        }
+
+
+        [TestMethod]
+        public void OperatorKleinerDanGeeftCorrecteBoolTerug() 
+        {
+            Vergelijking vergelijking = new Vergelijking(new ParameterTemperatuurKoudsteMaand(), Operator.KleinerDan, new ConstanteParameter(0));
+            bool oplossing = vergelijking.BerekenResultaat(_klimatogramMock.Object);
+            Assert.IsFalse(oplossing);
+
+        }
+        [TestMethod]
+        public void OperatorGroterDanGeeftCorrecteBoolTerug()
+        {
+            Vergelijking vergelijking = new Vergelijking(new ParameterTemperatuurWarmsteMaand(), Operator.GroterDan, new ConstanteParameter(20));
+            bool oplossing = vergelijking.BerekenResultaat(_klimatogramMock.Object);
+            Assert.IsTrue(oplossing);
+
+        }
+        [TestMethod]
+        public void OperatorKleinerDanOfGelijkAanGeeftCorrecteBoolTerug()
+        {
+            Vergelijking vergelijking = new Vergelijking(new ParameterTemperatuurKoudsteMaand(), Operator.KleinerDanOfGelijkAan, new ConstanteParameter(15));
+            bool oplossing = vergelijking.BerekenResultaat(_klimatogramMock.Object);
+            Assert.IsTrue(oplossing);
+
+        }
+        [TestMethod]
+        public void OperatorGroterDanOfGelijkAanGeeftCorrecteBoolTerug()
+        {
+            Vergelijking vergelijking = new Vergelijking(new ParameterTemperatuurKoudsteMaand(), Operator.GroterDanOfGelijkAan, new ConstanteParameter(12));
+            bool oplossing = vergelijking.BerekenResultaat(_klimatogramMock.Object);
+            Assert.IsFalse(oplossing);
+
+        }
+        [TestMethod]
+        public void OperatorNietGelijkAanGeeftCorrecteBoolTerug()
+        {
+            Vergelijking vergelijking = new Vergelijking(new ParameterTemperatuurKoudsteMaand(), Operator.NietGelijkAan, new ConstanteParameter(0));
+            bool oplossing = vergelijking.BerekenResultaat(_klimatogramMock.Object);
+            Assert.IsTrue(oplossing);
+
+        }
+    }
+}
